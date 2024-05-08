21a38bcb4b580a8fb3176366fc0daa059ecfb561
NicBJ <nbarahona19@alumnos.utalca.cl>
2024-05-07 23:53:36 -0400

    Joystick Controls Modification

--- Carreras/carrera/lib/interfaz_controles.dart
+++ Carreras/carrera/lib/interfaz_controles.dart
@@ -1,18 +1,14 @@
-// math y joystick adicionales
 import 'package:flutter/material.dart';
 import 'package:flutter_joystick/flutter_joystick.dart';
 import 'dart:math';
 
-// main
 void main() {
   runApp(const JoystickApp());
 }
 
-// Constantes globales para tamaños de widgets
 const joystickSize = 170.0;
 const actionButtonSize = 80.0;
 
-// Clase principal de la aplicación, define el "MaterialApp"
 class JoystickApp extends StatelessWidget {
   const JoystickApp({Key? key}) : super(key: key);
 
@@ -24,7 +20,6 @@ class JoystickApp extends StatelessWidget {
   }
 }
 
-// Widget principal que gestiona el estado de la aplicación
 class Control extends StatefulWidget {
   const Control({Key? key}) : super(key: key);
 
@@ -32,22 +27,21 @@ class Control extends StatefulWidget {
   State<Control> createState() => _ControlState();
 }
 
-// Estado de la aplicación, controla el estado del joystick y otros elementos
 class _ControlState extends State<Control> {
-  // Variables de estado para el joystick, modo, y slider
   double _joystickX = 20;
   double _joystickY = 0;
+  double _joystickX2 = 20;
+  double _joystickY2 = 0;
   JoystickMode _joystickMode = JoystickMode.all;
-  double _speedSliderValue = 0.0;
+  JoystickMode _joystickMode_vel = JoystickMode.vertical;
 
   @override
   Widget build(BuildContext context) {
-    // Construcción del Scaffold que define la interfaz de usuario
     return Scaffold(
       appBar: AppBar(
         title: const Text('Controles'),
         foregroundColor: Colors.white,
-        titleTextStyle: TextStyle(
+        titleTextStyle: const TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 30,
         ),
@@ -56,7 +50,6 @@ class _ControlState extends State<Control> {
       body: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          // Columna del Joystick
           Expanded(
             flex: 3,
             child: Padding(
@@ -66,27 +59,7 @@ class _ControlState extends State<Control> {
                   Positioned(
                     left: _joystickX,
                     bottom: _joystickY,
-                    child: GestureDetector(
-                      // Gestión de la interacción con el joystick
-                      onPanUpdate: (details) {
-                        double dx = details.localPosition.dx - (joystickSize / 2);
-                        double dy = details.localPosition.dy - (joystickSize / 2);
-                        double maxDistance = 0;
 
-                        // Calcular la distancia desde el centro del joystick
-                        double distance = sqrt(dx * dx + dy * dy);
-
-                        if (distance > maxDistance) {
-                          dx = (dx / distance) * maxDistance;
-                          dy = (dy / distance) * maxDistance;
-                        }
-
-                        // Actualizar la posición del joystick en el estado
-                        setState(() {
-                          _joystickX = dx + (joystickSize / 2);
-                          _joystickY = dy + (joystickSize / 2);
-                        });
-                      },
                       child: Container(
                         width: joystickSize,
                         height: joystickSize,
@@ -94,84 +67,49 @@ class _ControlState extends State<Control> {
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(joystickSize / 2),
                         ),
-                        // Widget de joystick proporcionado por flutter_joystick
                         child: Joystick(
                           mode: _joystickMode,
                           listener: (details) {
-                            // Lógica adicional del joystick si es necesaria
-
+                            // Additional logic for the first joystick if needed
                           },
                         ),
                       ),
                     ),
-                  ),
+
                 ],
               ),
             ),
           ),
-          // Columna de botones de acción y slider
           Expanded(
             flex: 2,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+              child: Stack(
                 children: [
-                  // Botón de acción (flecha hacia arriba)
-                  SizedBox(
-                    width: actionButtonSize,
-                    height: actionButtonSize,
-                    child: _buildActionButton(
-                      icon: Icons.arrow_circle_up,
-                      onPressed: () {
-                        print('Subir cambio');
-                      },
+                  Positioned(
+                    left: _joystickX2,
+                    bottom: _joystickY2,
+
+                      child: Container(
+                        width: joystickSize,
+                        height: joystickSize,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(joystickSize / 2),
+                        ),
+                        child: Joystick(
+                          mode: _joystickMode_vel,
+                          listener: (details) {
+                            // Additional logic for the second joystick if needed
+                          },
+                        ),
+                      ),
                     ),
-                  ),
-                  // Slider para ajustar la velocidad
-                  Slider(
-                    activeColor: Colors.red,
-                    value: _speedSliderValue,
-
-                    onChanged: (newValue) {
-                      setState(() {
-                        _speedSliderValue = newValue;
-                      });
-                    },
-
-                    min: 0,
-                    max: 100,
-                    divisions: 10,
-                    label: _speedSliderValue.toStringAsFixed(1),
-                  ),
                 ],
               ),
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  // Método para construir un botón de acción personalizado
-  Widget _buildActionButton({
-    required IconData icon,
-    required VoidCallback onPressed,
-  }) {
-    return InkWell(
-      onTap: onPressed,
-      borderRadius: BorderRadius.circular(actionButtonSize / 2),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.pink,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        child: Icon(
-          icon,
-          color: Colors.white,
-          size: actionButtonSize * 0.6,
-        ),
       ),
     );
   }
