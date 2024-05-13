0a7577b7d55d56e68d3583d0f6581c5d670b8b3d
Elias Ocque <elias.ocque2@gmail.com>
2024-05-13 12:33:53 -0400

    Arduino code to control robot

---
+++ Carreras/carrera_bt/carrera_bt.ino
@@ -0,0 +1,79 @@
+#include <SoftwareSerial.h>
+
+
+
+//Bluetooth Serial
+SoftwareSerial btSerial(10, 11); // RX, TX
+//Motor A
+#define AIN1 5
+#define AIN2 6
+//Motor B
+#define BIN1 9
+#define BIN2 3
+
+String Buffer = "";
+int left = 0;
+int right = 0;
+
+void motorL(int value) {
+  if ( value >= 0 ) {
+    analogWrite(BIN1, value);
+    analogWrite(BIN2, 0);
+  } else {
+    value *= -1;
+    analogWrite(BIN1, 0);
+    analogWrite(BIN2, value);
+  }
+}
+
+void motorR(int value) {
+  if ( value >= 0 ) {
+    analogWrite(AIN1, value);
+    analogWrite(AIN2, 0);
+  } else {
+    value *= -1;
+    analogWrite(AIN1, 0);
+    analogWrite(AIN2, value);
+  }
+}
+
+void motor(int left, int right) {
+  motorL(left);
+  motorR(right);
+}
+
+
+void setup() {
+  // Pin setup
+  pinMode(BIN2  , OUTPUT);
+  pinMode(BIN1  , OUTPUT);
+  pinMode(AIN1  , OUTPUT);
+  pinMode(AIN2  , OUTPUT);
+  btSerial.begin(9600);
+}
+
+void loop() {
+    while (btSerial.available()) {
+        char character = btSerial.read(); 
+        Buffer.concat(character); 
+        if (character == '\n') {
+            btSerial.print("Received: ");
+            btSerial.println(Buffer);
+
+            if (Buffer.length() > 2) {
+              String wheel = Buffer.substring(0, 1);
+              int speed = Buffer.substring(1, Buffer.length()-1).toInt();
+              if (speed > -255 && speed < 255) {
+                if (wheel == "L") {
+                  left = speed;
+                }
+                else if (wheel == "R") {
+                  right = speed;
+                }
+                motor(left, right);
+              }
+            }
+            Buffer = "";
+        }
+    }
+}
