3ed8bf8193bb73286a990bb7310e34fd342d88a2
Diego <dcaceres15@alumnos.utalca.cl>
2024-05-14 21:12:36 -0400

    Ajuste de interfaz para estar en linea con el diseño
    falta mover la caja de botones de la derecha un poco mas a la izquierda

--- Carreras/carrera/lib/interfaz_controles.dart
+++ Carreras/carrera/lib/interfaz_controles.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_joystick/flutter_joystick.dart';
-import 'dart:math';
 
 void main() {
   runApp(const JoystickApp());
@@ -14,7 +13,77 @@ class JoystickApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Control(),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'RoboRun',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        toolbarHeight: 20.0,
+      ),
+      body: Column(
+        children: [
+          ColoredBox(
+            color: Colors.white,
+            child: TabBar(
+              controller: _tabController,
+              tabs: [
+                Tab(icon: Icon(Icons.gamepad, color: Colors.black), text: 'Control'),
+                Tab(icon: Icon(Icons.settings, color: Colors.black), text: 'Configuración'),
+              ],
+              labelStyle: TextStyle(fontSize: 14.0),
+              labelColor: Colors.purple,
+              unselectedLabelColor: Colors.black,
+              indicatorColor: Colors.purple,
+            ),
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                Control(),
+                Configuracion(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -27,49 +96,24 @@ class Control extends StatefulWidget {
 }
 
 class _ControlState extends State<Control> {
-  //Posiciones de los Joystcik
   double _joystickX = 20;
   double _joystickY = 0;
-  double _joystickX2 = 20;
-  double _joystickY2 = 0;
-  JoystickMode _joystickMode = JoystickMode.all;
-  JoystickMode _joystickMode_vel = JoystickMode.vertical;
+  final JoystickMode _joystickMode = JoystickMode.all;
 
-  void _handleButtonPress() {
-    // Lógica a ejecutar cuando se presiona el botón
-    print('Botón presionado');
-  }
   void _onJoystickMove(StickDragDetails details) {
     double x = details.x;
     double y = details.y;
-    int speedX = (x * 255).toInt(); // Escalamos el valor de -1.0 a 1.0 a -255 a 255
-    int speedY = (y * 255).toInt(); // Escalamos el valor de -1.0 a 1.0 a -255 a 255
+    int speedX = (x * 255).toInt();
+    int speedY = (y * 255).toInt();
     String data = 'X$speedX Y$speedY\n';
-    //_sendData(data);
     print('Joystick : data: $data');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Controles'),
-        foregroundColor: Colors.white,
-        titleTextStyle: const TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 30,
-        ),
-        backgroundColor: Colors.teal,
-        actions: [
-          IconButton(
-
-            icon: Icon(Icons.settings),  // Icono del botón
-            onPressed: _handleButtonPress,  // Función que se ejecuta al presionar el botón,
-          ),
-        ],
-      ),
-      body: Row(
-
+    return Container(
+      color: Colors.white,
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
@@ -91,11 +135,6 @@ class _ControlState extends State<Control> {
                       child: Joystick(
                         mode: _joystickMode,
                         listener: _onJoystickMove,
-
-
-                        //listener: (details) {
-                          // Additional logic for the first joystick if needed
-
                       ),
                     ),
                   ),
@@ -104,36 +143,84 @@ class _ControlState extends State<Control> {
             ),
           ),
           Expanded(
-            flex: 2,
-
             child: Padding(
-              padding: const EdgeInsets.all(50.0),
-
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    bottom: _joystickY2,
-                    child: Container(
-                      width: joystickSize,
-                      height: joystickSize,
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(joystickSize / 2),
-                      ),
-                      child: Joystick(
-                        mode: _joystickMode_vel,
-                        listener: (details) {
-                          // Additional logic for the second joystick if needed
+              padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  color: Colors.grey[300],
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          print('Botón 3 presionado');
                         },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.pinkAccent,
+                        ),
+                        child: const Text(
+                          '3',
+                          style: TextStyle(
+                            color: Colors.white, // Cambiar color del texto a blanco
+                            fontSize: 18, // Aumentar el tamaño del texto
+                          ),
+                        ),
                       ),
-                    ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print('Botón 2 presionado');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.lightBlueAccent,
+                        ),
+                        child: const Text(
+                          '2',
+                          style: TextStyle(
+                            color: Colors.white, // Cambiar color del texto a blanco
+                            fontSize: 18, // Aumentar el tamaño del texto
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          print('Botón 1 presionado');
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black26,
+                        ),
+                        child: const Text(
+                          '1',
+                          style: TextStyle(
+                            color: Colors.white, // Cambiar color del texto a blanco
+                            fontSize: 18, // Aumentar el tamaño del texto
+                          ),
+                        ),
+                      ),
+
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+
+class Configuracion extends StatelessWidget {
+  const Configuracion({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: Text('Configuración', style: TextStyle(fontSize: 16)),
       ),
     );
   }
