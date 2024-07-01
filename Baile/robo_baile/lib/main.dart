3148152826d1cc8ff8eba5da74d7d64b55ff0a17
vejat <volea19@alumnos.utalca.cl>
2024-07-01 17:38:41 -0400

    merge completado

--- Baile/robo_baile/lib/main.dart
+++ Baile/robo_baile/lib/main.dart
@@ -4,10 +4,11 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:window_size/window_size.dart';
-import 'package:robobaile/ui/song_list.dart';
+import 'package:robobaile/ui/songs_list.dart';
 import 'package:robobaile/ui/device_list.dart';
 import 'package:robobaile/ui/music_player_state.dart';
 import 'package:robobaile/bluetooth/manager.dart';
+import 'package:robobaile/ui/music_player.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,9 +16,7 @@ void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => MusicPlayerState(),
-      child: MaterialApp(
-        home: HomePage(),
-      ),
+      child: const MyApp(),
     ),
   );
 }
@@ -33,9 +32,21 @@ void setupWindow() {
   }
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home:  HomePage(),
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
   final BluetoothManager manager = BluetoothManager();
-  HomePage({super.key});
+   HomePage({super.key});
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -43,11 +54,18 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
   late TabController _tabController;
+  bool isFullScreenPlayerVisible = false;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    final musicPlayerState = Provider.of<MusicPlayerState>(context, listen: false);
+    musicPlayerState.addListener(() {
+      setState(() {
+        isFullScreenPlayerVisible = musicPlayerState.isFullScreenPlayerVisible;
+      });
+    });
   }
 
   @override
@@ -59,43 +77,34 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
   @override
   Widget build(BuildContext context) {
     final musicPlayerState = Provider.of<MusicPlayerState>(context);
-
-    return MaterialApp(
-      theme: ThemeData.light(),
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.music_note, color: Colors.green), text: 'Música'),
-                Tab(icon: Icon(Icons.settings), text: 'Configuración'),
-              ],
-            ),
-            title: const Text('Robobaile'),
-          ),
-          body: Stack(
+    return Scaffold(
+      appBar: isFullScreenPlayerVisible
+          ? null
+          : AppBar(
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(icon: Icon(Icons.music_note, color: Colors.green), text: 'Música'),
+            Tab(icon: Icon(Icons.settings), text: 'Configuración'),
+          ],
+        ),
+        title: const Text('Robobaile'),
+      ),
+      body: Stack(
+        children: [
+          TabBarView(
+            controller: _tabController,
             children: [
-              TabBarView(
-                children: [
-                  SongList(),
-                  DeviceList(manager: widget.manager),
-                ],
-              ),
-              if (musicPlayerState.isPlaying && !musicPlayerState.isFullScreenPlayerVisible)
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: MusicPlayer(
-                    songTitle: musicPlayerState.currentSongTitle,
-                    artist: musicPlayerState.currentArtist,
-                    isPlaying: musicPlayerState.isPlaying,
-                    togglePlayPause: musicPlayerState.togglePlayPause,
-                    player: musicPlayerState.player,
-                  ),
-                ),
+              const SongList(),
+              DeviceList(manager: widget.manager),
             ],
           ),
-        ),
+          if (musicPlayerState.isPlaying && !musicPlayerState.isFullScreenPlayerVisible)
+            const Align(
+              alignment: Alignment.bottomCenter,
+              child: MiniPlayer(),
+            ),
+        ],
       ),
     );
   }
--- Baile/robo_baile/lib/ui/music_player_state.dart
+++ Baile/robo_baile/lib/ui/music_player_state.dart
@@ -1,17 +1,22 @@
 // ui/music_player_state.dart
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:robobaile/models/song.dart';
 
 class MusicPlayerState extends ChangeNotifier {
   final AudioPlayer player = AudioPlayer();
   bool isPlaying = false;
   String currentSongTitle = '';
   String currentArtist = '';
+  String currentSongUrl = '';
   bool isFullScreenPlayerVisible = false;
+  List<Song> songs = [];
+  int currentIndex = -1;
 
   void playSong(String title, String artist, String songUrl) async {
     currentSongTitle = title;
     currentArtist = artist;
+    currentSongUrl = songUrl;
     await player.setUrl(songUrl);
     player.play();
     isPlaying = true;
@@ -28,8 +33,38 @@ class MusicPlayerState extends ChangeNotifier {
     notifyListeners();
   }
 
+  void stopPlaying() {
+    player.stop();
+    isPlaying = false;
+    notifyListeners();
+  }
+
   void setFullScreenPlayerVisible(bool isVisible) {
     isFullScreenPlayerVisible = isVisible;
+    notifyListeners();
+  }
+
+  void playNext() {
+    if (currentIndex < songs.length - 1) {
+      currentIndex++;
+      playSong(songs[currentIndex].title, songs[currentIndex].artist, songs[currentIndex].songUrl);
+    }
+  }
+
+  void playPrevious() {
+    if (currentIndex > 0) {
+      currentIndex--;
+      playSong(songs[currentIndex].title, songs[currentIndex].artist, songs[currentIndex].songUrl);
+    }
+  }
+
+  void addSong(Song newSong) {
+    songs.add(newSong);
+    notifyListeners();
+  }
+
+  void setSongs(List<Song> newSongs) {
+    songs = newSongs;
     notifyListeners();
   }
 }
